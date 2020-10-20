805a26ecb148a08742af65f3c664e645bd127b68
Sebastian Nyberg <sebastian.nyberg@securitas.com>
2020-10-20 09:35:07 +0200

    add missing tee to bashrc fetch

--- .devcontainer/scripts/debian.sh
+++ .devcontainer/scripts/debian.sh
@@ -86,7 +86,8 @@ EOF
 
 # Basic .bashrc with prompt etc
 if [ "${INIT_RC}" = "true" ]; then
-    curl https://gist.githubusercontent.com/sebnyberg/3d3f55588f70c897a2c8c7306e5fd3ea/raw/7576457445d96a87d91568261ae348caa308750f/.bashrc
+    curl https://gist.githubusercontent.com/sebnyberg/3d3f55588f70c897a2c8c7306e5fd3ea/raw/7576457445d96a87d91568261ae348caa308750f/.bashrc \
+        | tee -a /etc/bash.bashrc
 fi
 
 # Set Locale
